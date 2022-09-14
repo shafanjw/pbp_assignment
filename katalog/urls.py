698590af4881ab31e8138de87f9dcbb333b5dc29
shafanjw <shafa.najwa@ui.ac.id>
2022-09-14 16:15:21 +0700

    Add function in views, do routinig, do migrations for catalog

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
-# TODO: Implement Routings Here
+from katalog.views import show_catalogs
+from django.urls import path
+
+app_name = "katalog"
+
+urlpatterns = [
+    path("", show_catalogs, name="show_catalogs"),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalogs(request) :
+    items = CatalogItem.objects.all()
+    context = {
+        "items": items,
+        "student_name": "Shafa Najwa Nathania",
+        "student_id": 2106634414,
+    }
+    return render(request, "katalog.html", context)
