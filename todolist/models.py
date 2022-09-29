59999513f7ec2608783f0d24d0e02edf38b0f81c
shafanjw <shafa.najwa@ui.ac.id>
2022-09-29 10:23:34 +0700

    fix user login error

--- todolist/models.py
+++ todolist/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     date = models.DateField()
     title = models.CharField(max_length=255)
     description = models.TextField()
